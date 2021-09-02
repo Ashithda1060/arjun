2a5c1333f5a4d6c9b493e010ce9cbadf50ab429d
Ashida <ashidada.xworkz@gamil.com>
2021-09-02 21:50:56 +0530

    added new file

---
+++ Vaccine.java
@@ -0,0 +1,14 @@
+class Vaccine
+{
+static String vaccinesName;
+static double vaccinesPrice;
+static String personName;
+ public static void main(String[]agrs){
+   vaccinesName ="covisheild";
+   vaccinesPrice =250;
+   personName ="Ramesh";
+System.out.println("vaccines name is " +vaccinesName);
+System.out.println("person name is " +personName);
+System.out.println("vaccines price is " +vaccinesPrice);
+          }
+}
