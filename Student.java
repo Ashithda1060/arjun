798236cd75dae4def0a0f18c94cbaa1efb74a3ff
Ashida <ashidada.xworkz@gamil.com>
2021-09-02 22:03:37 +0530

    added new java file

---
+++ Student.java
@@ -0,0 +1,17 @@
+class Student
+{
+static int usn;
+static double studentFee;
+static char studentSec;
+static String studentName;
+ public static void main(String[]agrs){
+   usn =123;
+   studentFee =250000;
+   studentSec ='A';
+   studentName ="Ramesh";
+System.out.println("student usn is " +usn);
+System.out.println("student name is " +studentName);
+System.out.println("student fee is " +studentFee);
+System.out.println("student section is " +studentSec);
+          }
+}
