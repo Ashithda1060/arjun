4bd47532d80765eacc2f9b75c13bdde3c5599c5c
Ashida <ashidada.xworkz@gamil.com>
2021-09-02 21:45:29 +0530

    added new java file

---
+++ Product.java
@@ -0,0 +1,14 @@
+class Product
+{
+static String productName;
+static double productPrice;
+static String personName;
+ public static void main(String[]agrs){
+   productName ="one+nord";
+   productPrice =30000;
+   personName ="Ramesh";
+System.out.println("product name is " +productName);
+System.out.println("person name is " +personName);
+System.out.println("product price is " +productPrice);
+          }
+}
