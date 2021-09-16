d6b2ee34df34db337cc63185c96ffeebb411539f
Ashithda1060 <89936083+Ashithda1060@users.noreply.github.com>
2021-09-16 22:48:14 +0530

    16/09

---
+++ SpeakerUtil.java
@@ -0,0 +1,18 @@
+class SpeakerUtil {
+
+
+ public static void main(String a[]){
+
+
+boolean isConnected= Speaker.onOrOff();
+System.out.println("the Speaker is Connected "+isConnected);
+
+Speaker.increaseVolume();
+Speaker.increaseVolume();
+Speaker.increaseVolume();
+Speaker.increaseVolume();
+Speaker.increaseVolume();
+
+
+}
+}
